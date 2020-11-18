c698122d205fe8524eb5803fedeb1ae145d67532
eunjin0212 <eunjin9639@gmail.com>
2020-11-18 21:55:35 +0900

    CircularQueue

--- qu.js
+++ qu.js
@@ -1,34 +1,90 @@
-class Qu {
+class LinearQu {
   arr = [];
-  first = 0;
-  last = 0;
+  front = 0;
+  rear = 0;
   capacity = 10;
 
   queue(data) {
     if (this.arr.length > this.capacity) {
       throw new Error("Over capacity");
     }
-    this.arr[this.last] = data;
-    this.last++;
+    this.arr[this.rear] = data;
+    this.rear++;
   }
   dequeue() {
-    let data = this.arr[this.first];
-    delete this.arr[this.first];
-    this.first++;
+    let data = this.arr[this.front];
+    delete this.arr[this.front];
+    this.front++;
     return data;
   }
   show() {
     console.log("show " + this.arr);
   }
 }
-function main() {
-  const qu = new Qu();
-  qu.queue(10);
-  qu.queue(20);
-  qu.queue(30);
+function quMain() {
+  const linearQu = new LinearQu();
+  linearQu.queue(10);
+  linearQu.queue(20);
+  linearQu.queue(30);
 
-  qu.dequeue();
+  linearQu.dequeue();
 
-  qu.show();
+  linearQu.show();
 }
-main();
+//quMain();
+
+class CircularQueue {
+  constructor() {
+    this.maxQueueSize = 7;
+    this.array = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+  Empyt() {
+    return this.front === this.rear;
+  }
+  Full() {
+    return (this.rear + 1) % this.maxQueueSize === this.front;
+  }
+  Queue(data) {
+    if (this.Full()) {
+      throw new Error("Over capacity");
+    } else {
+      this.array[(this.rear = (this.rear + 1) % this.maxQueueSize)] = data;
+      return this.array;
+    }
+  }
+  DeQueue() {
+    if (this.Empyt()) {
+      throw new Error("Queue is Empty");
+    } else {
+      let item = this.array[
+        (this.front = (this.front + 1) % this.maxQueueSize)
+      ];
+      delete this.array[this.front % this.maxQueueSize];
+      return item;
+    }
+  }
+  ShowQu() {
+    console.log(this.array);
+  }
+}
+function circularMain() {
+  const circularQueue = new CircularQueue();
+  circularQueue.Queue(1);
+  circularQueue.Queue(2);
+  circularQueue.Queue(3);
+  circularQueue.Queue(4);
+  circularQueue.Queue(5);
+  circularQueue.Queue(6);
+
+  circularQueue.ShowQu();
+
+  circularQueue.DeQueue();
+  circularQueue.DeQueue();
+  circularQueue.DeQueue();
+  circularQueue.DeQueue();
+
+  circularQueue.ShowQu();
+}
+circularMain();
