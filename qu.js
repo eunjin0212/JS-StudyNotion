31eccd0d57fc2bbae639524d7e092c443d4b50c4
eunjin0212 <eunjin9639@gmail.com>
2020-11-18 18:44:25 +0900

    dequeue

--- qu.js
+++ qu.js
@@ -1,32 +1,34 @@
 class Qu {
   arr = [];
-  rear = 0;
+  first = 0;
+  last = 0;
   capacity = 10;
+
   queue(data) {
     if (this.arr.length > this.capacity) {
       throw new Error("Over capacity");
     }
-    this.arr[this.rear++] = data;
+    this.arr[this.last] = data;
+    this.last++;
   }
   dequeue() {
-    if (this.arr.length <= 0) {
-      throw new Error("The End");
-    }
-    return this.arr.shift();
+    let data = this.arr[this.first];
+    delete this.arr[this.first];
+    this.first++;
+    return data;
   }
   show() {
-    console.log(this.arr);
+    console.log("show " + this.arr);
   }
 }
 function main() {
   const qu = new Qu();
   qu.queue(10);
   qu.queue(20);
-  qu.queue(30);
-  qu.show();
+  qu.queue(20);
 
-  console.log(qu.dequeue());
-  console.log(qu.dequeue());
-  console.log(qu.dequeue());
+  qu.dequeue();
+
+  qu.show();
 }
 main();
